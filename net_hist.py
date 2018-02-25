f752c6a7a6738093e8d18302ac3b6d21230349d3
ALEXIS STARR <astarr8181@gmail.com>
2018-02-25 18:52:12 -0500

    update

--- net_hist.py
+++ net_hist.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import re
 import numpy as np
+import os
 
 Data_root = 'Data'
 Data_name = 'bot_net.csv'
@@ -16,5 +17,17 @@ df = pd.DataFrame.from_csv(Path_to_Data)
 df.columns = ['Date','Local_IP', 'ASN', 'Flows']
 
 list_dates = df.Date.unique()
-print(list_dates)
+ips = df.Local_IP.unique()
+
+mean_flows= df.groupby(['Date','Local_IP'], as_index=False)['Flows'].mean()
+median_flows= df.groupby(['Date', 'Local_IP'], as_index=False)['Flows'].median()
+
+df_mean = pd.DataFrame(mean_flows)
+df_med = pd.DataFrame(median_flows)
+df_mean_med_flows = pd.concat([mean_flows, median_flows], axis=1)
+df_mean_med_flows.columns = ['Date','Local_IP', 'Flows_Mean', 'Date1','IP1', 'Flows_Median']
+df_mean_med_flows.drop(['Date1', 'IP1'], axis=1)
+
+#pd.df_mean_med.to_csv(Path_to_Data)
+#print(Path_to_Data)
 
